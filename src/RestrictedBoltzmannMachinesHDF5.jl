bce5186d48f3869e4e8398297ce485564e9005fc
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-06-14 10:33:16 +0200

    format version

--- src/RestrictedBoltzmannMachinesHDF5.jl
+++ src/RestrictedBoltzmannMachinesHDF5.jl
@@ -3,6 +3,12 @@ module RestrictedBoltzmannMachinesHDF5
 using HDF5: h5open
 using RestrictedBoltzmannMachines: RBM, Binary, Potts, xReLU
 
+# Version of the file format used to save/load RBMs
+const FILE_FORMAT_VERSION = v"1.0.0"
+
+# Header used to identify the file format in the HDF5 file structure
+const FILE_FORMAT_HEADER = "rbm_hdf5_file_format_version"
+
 include("io.jl")
 include("layers.jl")
 
--- src/io.jl
+++ src/io.jl
@@ -9,7 +9,8 @@ function save_rbm(path::AbstractString, rbm::RBM; overwrite::Bool=false)
         throw(IOError("File already exists: $path"))
     end
     h5open(path, "w") do file
-        write(file, "w", rbm.w)
+        write(file, FILE_FORMAT_HEADER, string(FILE_FORMAT_VERSION))
+        write(file, "weights", rbm.w)
         write(file, "visible_par", rbm.visible.par)
         write(file, "hidden_par", rbm.hidden.par)
         write(file, "visible_type", layer_type(rbm.visible))
@@ -24,8 +25,12 @@ end
 Load an RBM from an HDF5 file at `path`.
 """
 load_rbm(path::AbstractString) = h5open(path, "r") do file
-    w = read(file, "w")
-    visible = construct_layer(read(file, "visible_type"), read(file, "visible_par"))
-    hidden = construct_layer(read(file, "hidden_type"), read(file, "hidden_par"))
-    return RBM(visible, hidden, w)
+    if read(file, FILE_FORMAT_HEADER) == string(FILE_FORMAT_VERSION)
+        w = read(file, "weights")
+        visible = construct_layer(read(file, "visible_type"), read(file, "visible_par"))
+        hidden = construct_layer(read(file, "hidden_type"), read(file, "hidden_par"))
+        return RBM(visible, hidden, w)
+    else
+        throw(IOError("File format version mismatch"))
+    end
 end
